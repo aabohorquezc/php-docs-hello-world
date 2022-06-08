2afcc212c7a71e99b43762d8a4b9b8b15ed3501e
aabohorquezc <96751141+aabohorquezc@users.noreply.github.com>
2022-06-07 22:33:56 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
 <?php
-
+$image = 'https://i.pinimg.com/736x/7a/9c/fb/7a9cfb9d6a0c58904d3c980af64400d0.jpg';
+$imageData = base64_encode(file_get_contents($image));
+echo '<img src="data:image/jpeg;base64,'.$imageData.'">';
 echo "Wu Ji Bi Fan que significa vamos a dormir mi chitita hermosa te amo";
+?>
